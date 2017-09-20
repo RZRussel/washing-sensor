12ad1e9796ac798b2ebfb28e81ab8f595ce1d861
Russel <emkil.russel@gmail.com>
2017-09-20 03:06:50 +0300

    [new] saveVibration api function added

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,32 @@
+// error codes
+const CODE_MISSING_PARAMETER = 1;
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('saveVibration', function(req, res) {
+	if (!req.params["vibration"]){
+		failResponse(res, CODE_MISSING_PARAMETER, "no vibration value found");
+		return;
+	}
+
+	address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	vibration = req.params["vibration"];
+
+	var measurement = new Parse.Object("Measurement");
+	measurement.set("address", address);
+	measurement.set("vibration", vibration);
+	measurement.save(null, {
+		useMasterKey: true,
+		success: function(object){
+			res.success(object);
+		},
+		error: function(object, error){
+			failResponse(res, error.code, error.message);
+		}
+	});
 });
+
+function failResponse(response, code, info){
+	response.error({
+		"code" : code,
+		"info" : info
+	});
+}
